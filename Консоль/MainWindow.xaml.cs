d87d43896a04c7db282910a3cae32518a50b95bd
mironov1105 <77550644+mironov1105@users.noreply.github.com>
2022-02-04 16:19:34 +1000

    Update MainWindow.xaml.cs

--- Консоль/MainWindow.xaml.cs
+++ Консоль/MainWindow.xaml.cs
@@ -17,30 +17,48 @@ namespace UserApp
 
         private void Button_Reg_Click(object sender, RoutedEventArgs e)
         {
-            string login = textBoxLogin.Text;
-            string pass = textBoxPass.Password;
-            string pass2 = textBoxPass2.Password;
+            string login = textBoxLogin.Text.Trim();
+            string pass = textBoxPass.Password.Trim();
+            string pass2 = textBoxPass2.Password.Trim();
             string email = Email.Text;
-           
-            try
-            {
-                if (login.Length > 3 && pass == pass2 && pass.Length > 5 && email.Contains("@") && email.Contains("."))
-                {
-                    Users user = new Users(login, pass, email);
-                    db.Add(user);
-                    userPage mainW = new userPage();
-                    mainW.Show();
-                    Hide();
-                }
-                else
-                {
-                    MessageBox.Show("error");
-                }
 
-            }
-            catch (Exception ex)
+            textBoxLogin.ToolTip = "";
+            textBoxLogin.Background = Brushes.Transparent;
+            textBoxPass.ToolTip = "";
+            textBoxPass.Background = Brushes.Transparent;
+
+
+            if (login.Length < 5)
             {
-                MessageBox.Show(ex.Message);
+                textBoxLogin.ToolTip = "Incorrect input";
+                textBoxLogin.Background = Brushes.DarkRed;
+            }
+            else if (pass.Length < 3 || pass.Length > 10)
+            {
+                textBoxPass.ToolTip = "Incorrect input";
+                textBoxPass.Background = Brushes.DarkRed;
+            }
+            else if (pass2 != pass)
+            {
+                textBoxPass2.ToolTip = "Incorrect input";
+                textBoxPass2.Background = Brushes.DarkRed;
+            }
+            else if (!email.Contains("@") || !email.Contains("."))
+            {
+                Email.ToolTip = "Incorrect input";
+            }
+            else
+            {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+                textBoxPass.ToolTip = "";
+                textBoxPass.Background = Brushes.Transparent;
+
+                Users user = new Users(login, pass, email);
+                db.Add(user);
+                userPage us = new userPage();
+                us.Show();
+                Hide();
             }
         }
 
